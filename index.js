f886422b4d32dd139ac7856ae9714896f677383b
Xheldon <c1006044256@gmail.com>
2018-11-14 20:08:17 +0800

    update something

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 console.log('success render!');
-const Vue = require('vue')
-const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+const Vue = require('vue');
+const server = require('express')();
+const renderer = require('vue-server-renderer').createRenderer();
 server.get('*', (req, res) => {
-  const app = new Vue({
-    data: {
-      url: req.url
-    },
-    template: `<div>You visited url is: {{ url }}</div>`
-  })
-  renderer.renderToString(app, (err, html) => {
-    if (err) {
-      res.status(500).end('Internal Server Error')
-      return
-    }
-    res.end(`
+    const app = new Vue({
+        data: {
+            url: req.url
+        },
+        template: `<div>You visited url is: {{ url }}</div>`
+    });
+    renderer.renderToString(app, (err, html) => {
+        if (err) {
+            res.status(500).end('Internal Server Error')
+            return
+        }
+        res.end(`
       <!DOCTYPE html>
       <html lang="en">
         <head><title>Hello</title></head>
         <body>${html}</body>
       </html>
     `)
-  })
-})
-server.listen(8080)
+    })
+});
+server.listen(8080);
